f60cfcbec03e9f52f19fc2cb41b0a574ec9bd95b
Gerson Severo <gersonsevero90@gmail.com>
2021-07-02 20:18:37 -0300

    Database connection

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -4,6 +4,8 @@ import path from "path";
 const connection = knex({
   client: "sqlite3",
   connection: {
-    filename: path.resolve(),
+    filename: path.resolve(__dirname, "database.sqlite"),
   },
 });
+
+export default connection;
