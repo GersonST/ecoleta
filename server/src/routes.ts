49506e820489d1ee60363c029a4446f108b1cc20
Gerson Severo <gersonsevero90@gmail.com>
2021-07-03 19:24:10 -0300

    Controllers
    Criação dos controladores e organização das rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,51 +1,14 @@
 import express from "express";
-import knex from "./database/connection";
 
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
+
+// index, show, create, update, delete
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get("/items", async (request, response) => {
-  const items = await knex("items").select("*");
-
-  const serializedItem = items.map((item) => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    };
-  });
-
-  return response.json(serializedItem);
-});
-
-routes.post("/points", async (request, response) => {
-  const { name, email, whatsapp, latitude, longitude, city, uf, items } =
-    request.body;
-
-  const trx = await knex.transaction();
-
-  const insertedIds = await trx("points").insert({
-    image: "image-fake",
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-  });
-
-  const point_id = insertedIds[0];
-
-  const pointItems = items.map((item_id: number) => {
-    return {
-      item_id,
-      point_id,
-    };
-  });
-
-  await trx("point_items").insert(pointItems);
-
-  return response.json({ success: true });
-});
+routes.get("/items", itemsController.index);
+routes.post("/points", pointsController.create);
 
 export default routes;
