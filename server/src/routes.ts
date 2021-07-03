d2bde199de8dee70f85e9cd08ee7ee63a19ffcf3
Gerson Severo <gersonsevero90@gmail.com>
2021-07-03 18:45:37 -0300

    Points ready
    Cadastro de pontos de coleta finalizado

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -21,7 +21,9 @@ routes.post("/points", async (request, response) => {
   const { name, email, whatsapp, latitude, longitude, city, uf, items } =
     request.body;
 
-  await knex("points").insert({
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx("points").insert({
     image: "image-fake",
     name,
     email,
@@ -31,6 +33,17 @@ routes.post("/points", async (request, response) => {
     city,
     uf,
   });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id,
+    };
+  });
+
+  await trx("point_items").insert(pointItems);
 
   return response.json({ success: true });
 });
