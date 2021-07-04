70a41b32cd489014ceb8e8e3dc7bde44238c1d7f
Gerson Severo <gersonsevero90@gmail.com>
2021-07-04 16:11:38 -0300

    Header Component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-
 import "./App.css";
 
+import Header from "./Header";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div>
+      <Header />
+      <h1>Conteúdo do App</h1>
+    </div>
+  );
 }
 
 export default App;
