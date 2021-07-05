bf38ebc973e34f4db55ae3e1b7e7eefd747b4740
Gerson Severo <gersonsevero90@gmail.com>
2021-07-05 03:59:46 -0300

    Get items from Api

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -106,7 +106,7 @@ const CreatePoint = () => {
           <ul className="items-grid">
             {items.map((item) => {
               return (
-                <li>
+                <li key={item.id}>
                   <img src={item.image_url} alt={item.title} />
                   <span>{item.title}</span>
                 </li>
