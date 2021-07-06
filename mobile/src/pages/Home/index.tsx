ca91f2896e4fda73a0d8e9a3bc3fb4100b3256bf
Gerson Severo <gersonsevero90@gmail.com>
2021-07-06 01:19:14 -0300

    Routes Done

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import { View, ImageBackground, Image, StyleSheet, Text } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 import { Feather as Icon } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/core";
 
 const Home = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateToPoints() {
+    navigation.navigate("Points");
+  }
+
   return (
     <ImageBackground
       source={require("../../assets/home-background.png")}
@@ -19,7 +26,7 @@ const Home = () => {
       </View>
 
       <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={() => {}}>
+        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
           <View style={styles.buttonIcon}>
             <Text>
               <Icon name="arrow-right" color="#FFF" size={24} />
@@ -36,7 +43,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    backgroundColor: "#f0f0f5",
   },
 
   main: {
